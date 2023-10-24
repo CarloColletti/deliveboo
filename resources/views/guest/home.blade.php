90d1472c481065aa0186df397fca6e8ec1d7612c
Aurora-Bizzo <aurora.bizzozero98@gmail.com>
2023-10-24 15:30:08 +0200

    styling login e register provvisorio

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -4,7 +4,8 @@
     <div class="container">
       <div class="row">
         <div class="col-6">
-          <h1>Unisciti al nostro team!</h1>
+          <h1 class="color-purple">Unisciti al nostro team!</h1>
+          <h4 class="color-purple lh-lg pt-3">Più di 160.000 partner, 90% dei ristoranti nella nostra piattaforma ha iniziato con il delivery proprio grazie a noi, , spalancando le porte a un mondo di opzioni e di gusto per i clienti di tutto il mondo.</h4>
         </div>
         <div class="col-6">
           <img src="https://a.storyblok.com/f/62776/668x608/56d8990dbd/biker.png" alt="" style="width: 100%">
