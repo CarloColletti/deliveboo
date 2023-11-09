90dbd2eca0cca55de95ccc9bef7ffb459869d9cb
Christian01881 <chrispamo01881@gmail.com>
2023-11-09 18:12:41 +0100

    add order api controller

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\Order;
-use App\Http\Requests\StoreOrderRequest;
+use Illuminate\Http\Request;
 use App\Http\Requests\UpdateOrderRequest;
+use Illuminate\Support\Facades\Log;
 
 class OrderController extends Controller
 {
@@ -34,9 +36,49 @@ class OrderController extends Controller
      * @param  \App\Http\Requests\StoreOrderRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreOrderRequest $request)
+    public function store(Request $request)
     {
-        //
+        // $formData = $request->all();
+        // $newOrder = new Order;
+        // $newOrder->fill($formData);
+        // $newOrder->save();  
+        // if(empty($formData)) {
+    
+        //     return response()->json([
+    
+        //         'success' => false,
+        //         'error' => 'Questo progetto non esiste!'
+        //     ]);
+        
+        // } else {
+    
+        //     return response()->json([
+        //         'success' => true,
+        //         'results' => $formData,
+                  
+        //     ]);
+    
+        // }
+
+        try {
+            $formData = $request->all();
+    
+            $newOrder = new Order;
+            $newOrder->fill($formData);
+            $newOrder->save();
+    
+            return response()->json([
+                'success' => true,
+                'results' => $formData,
+            ]);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return response()->json([
+                'success' => false,
+                'error' => 'Errore interno del server',
+                'details' => $e->getMessage(),
+            ], 500);
+        }
     }
 
     /**
